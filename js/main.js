54d13d114e27390f98fc62d7344702c04ca80100
ADRIEL LOURENÇO <21276092@CFP127ALUNO.LOCAL>
2022-02-09 17:01:35 -0300

    Primeiro Commit

---
+++ js/main.js
@@ -0,0 +1,61 @@
+'use strict'
+
+//Declaração das Variáveis dos Elementos HTML
+const semaforo = document.getElementById('semaforo')
+const sinalVerde = document.getElementById('sinalVerde')
+const sinalAmarelo = document.getElementById('sinalAmarelo')
+const sinalVermelho = document.getElementById('sinalVermelho')
+const automatico = document.getElementById('automatico')
+let idPiscar 
+
+const tornarSinalVerde = () => {
+    semaforo.src = './imgs/verde.png'
+}
+
+const tornarSinalAmarelo = () => {
+    semaforo.src = './imgs/amarelo.png'
+}
+
+const tornarSinalVermelho = () => {
+    semaforo.src = './imgs/vermelho.png'
+}
+
+const estaVerde = () => {
+    return semaforo.src.includes('verde')
+}
+
+const estaAmarelo = () => {
+    return semaforo.src.includes('amarelo')
+}
+
+const estaVermelho = () => {
+    return semaforo.src.includes('vermelho')
+}
+
+const trocarCor = () => {
+    if (estaVerde()) {
+        tornarSinalAmarelo()
+    } else if (estaAmarelo()) {
+        tornarSinalVermelho()
+    } else if (estaVermelho()) {
+        tornarSinalVerde()
+    }
+}
+
+const tornarAutomatico = () => {
+    idPiscar = setInterval(trocarCor, 1000)
+}
+
+sinalVerde.addEventListener('click', () => {
+    clearInterval(idPiscar)
+    tornarSinalVerde()
+})
+sinalAmarelo.addEventListener('click', () => {
+    clearInterval(idPiscar)
+    tornarSinalAmarelo()
+})
+sinalVermelho.addEventListener('click', () => {
+    clearInterval(idPiscar)
+    tornarSinalVermelho()
+})
+automatico.addEventListener('click', tornarAutomatico) 
